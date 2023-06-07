e0eca9e826e2f37920169692665e0dd00aa6999c
MarioGogogo <148373644@qq.com>
2023-06-07 23:21:05 +0800

    feat:将logs表拆开

---
+++ app/model/action-logs.js
@@ -0,0 +1,61 @@
+module.exports = (app) => {
+  const mongoose = app.mongoose;
+  const Schema = mongoose.Schema;
+  const ActionLogsSchema = new Schema({
+    timestamp: {
+      type: String,
+      require: true,
+    },
+    reportTime: {
+      type: String,
+      require: true,
+    },
+    projectName: {
+      type: String,
+      require: true,
+    },
+    host: {
+      type: String,
+      require: true,
+    },
+    url: {
+      type: String,
+      require: true,
+    },
+    userAgent: {
+      type: String,
+      require: true,
+    },
+    client: {
+      type: String,
+      require: true,
+    },
+    browser: {
+      type: String,
+      require: true,
+    },
+    type: {
+      type: String,
+      require: true,
+    },
+    errorType: {
+      type: String,
+    },
+    pathname: {
+      type: String,
+    },
+    status: {
+      type: String,
+    },
+    duration: {
+      type: String,
+    },
+    request: {
+      type: String,
+    },
+    response: {
+      type: String,
+    },
+  });
+  return mongoose.model('ActionLogs', ActionLogsSchema, 'action_logs');
+};
---
+++ app/model/error-logs.js
@@ -0,0 +1,67 @@
+module.exports = (app) => {
+  const mongoose = app.mongoose;
+  const Schema = mongoose.Schema;
+  const ErrorLogsSchema = new Schema({
+    timestamp: {
+      type: String,
+      require: true,
+    },
+    reportTime: {
+      type: String,
+      require: true,
+    },
+    projectName: {
+      type: String,
+      require: true,
+    },
+    host: {
+      type: String,
+      require: true,
+    },
+    url: {
+      type: String,
+      require: true,
+    },
+    userAgent: {
+      type: String,
+      require: true,
+    },
+    client: {
+      type: String,
+      require: true,
+    },
+    browser: {
+      type: String,
+      require: true,
+    },
+    type: {
+      type: String,
+      require: true,
+    },
+    errorType: {
+      type: String,
+    },
+    message: {
+      type: String,
+    },
+    filename: {
+      type: String,
+    },
+    tagName: {
+      type: String,
+    },
+    selector: {
+      type: String,
+    },
+    stack: {
+      type: String,
+    },
+    lineno: {
+      type: String,
+    },
+    colno: {
+      type: String,
+    },
+  });
+  return mongoose.model('ErrorLogs', ErrorLogsSchema, 'error_logs');
+};
